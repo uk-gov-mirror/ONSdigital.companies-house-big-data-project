a2bf03f41cbe7fd5781b82884de1d3ad20ae55c9
Robert Stone <robert.stone@ext.ons.gov.uk>
2020-03-01 14:52:31 +0000

    The code will now take as input a directory of images (-d “path” at command line). All images processed by the classifier are saved to a directory, which specified by the user (-p “path” at command line). The labels of every processed image is equal to the labels of all images offered as input to the classifier. The user also has the option to visually inspect all processed images by specifying -v at the command line and setting a value of “True“. Once an image is displayed, press “q” to close the image frame down and move on to visualise the next image being processed.

--- src/cascade_classifier.py
+++ src/cascade_classifier.py
@@ -16,27 +16,15 @@ def imgs_to_grey(image):
 
 def classifier_input(path):
     '''
-    cv_img = []
-    for img in path:
-        n = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
-        cv_img.append(n)
-		
-	import os
-import cv2
-path_of_images = r"E:\companies_house_project\repos\companies_house_project\data\input"
-list_of_images = os.listdir(path_of_images)
-for image in list_of_images:
-    img = cv2.imread(os.path.join(path_of_images, image))
     '''
-#    return cv2.imread(path)
-	return imread_collection(path)
+    return cv2.imread(path)
 
 def classifier(detector, imgs, scale_factor, min_neighbors, w, h):
     '''
     '''
     return detector.detectMultiScale(imgs, scale_factor, min_neighbors, minSize = (w, h))
 
-def classifier_outputdd(title, imgs):
+def classifier_output_show(title, imgs):
     '''
     '''
     return cv2.imshow(title, imgs)
@@ -57,15 +45,15 @@ def classifier_output(rects, image, path):
         cv2.imwrite(path + str(w) + str(h) + "_ch_stamps.png", roi_colour)
         
 def classifier_process(image, path):
-    status = cv2.imwrite(path + "test.png", image)
+    status = cv2.imwrite(path, image)
     print("[INFO] Output from the classifier written to filesystem in: " + path, "[STATUS]: " + str(status))
     
 def main():
     '''
-	
-    EXE on Mac " python3 cascade_classifier.py -d ../data/input/test_img_04.tif -c ../data/cascade_files/chp_stamp_cascade_classifier_20200224.xml -s 1.2 -m 10 -x 60 -y 30 -o ../data/output/classifier_output/ -p ../data/output/processed_images/ "
- 
-	EXE on a Windows machine " python cascade_classifier.py -d ..\data\input\test_img_04.tif -c ..\data\cascade_files\chp_stamp_cascade_classifier_20200224.xml -s 1.2 -m 10 -x 60 -y 30 -o ..\data\output\classifier_output\ -p ..\data\output\processed_images\ "
+
+    EXE on Mac " python3 cascade_classifier.py -d ../data/input/test_img_04.tif -c ../data/cascade_files/chp_stamp_cascade_classifier_20200224.xml -s 1.2 -m 10 -x 60 -y 30 -o ../data/output/classifier_output/ -p ../data/output/processed_images -v False/ "
+
+    EXE on a Windows machine " python cascade_classifier.py -d ..\data\input\test_img_04.tif -c ..\data\cascade_files\chp_stamp_cascade_classifier_20200224.xml -s 1.2 -m 10 -x 60 -y 30 -o ..\data\output\classifier_output\ -p ..\data\output\processed_images\ -v False"
     
     '''
     
@@ -87,30 +75,39 @@ def main():
         help = "path to classifier output.")
     ap.add_argument("-p", "--processed_images_dir", required = True,
         help = "path to images processed by the classifier.")
+    ap.add_argument("-v", "--show_classifier_output", required = False,
+        help = "display the output of the classifier to the user.")
     args = vars(ap.parse_args())
 
-    # load the input image and convert it to grayscale
-    image = classifier_input(args["input_imgs"])
-    gray = imgs_to_grey(image)
-
-    #
     detector = cv2.CascadeClassifier(args["cascade_file"])
-    rects = classifier(detector,
-                       gray,
-                       float(args["scale_factor"]),
-                       int(args["min_neighbors"]),
-                       int(args["cascade_width"]),
-                       int(args["cascade_height"]))
 
-    print("[INFO] Found " + str(classifier_output_count(rects)) + " companies house stamps.\n")
+    # load the input image and convert it to grayscale
+    for image in listdir(args["input_imgs"]):
+        img = classifier_input(join(args["input_imgs"], image))
+        grey = imgs_to_grey(img)
 
-    #
-    classifier_output(rects, image, args["classifier_output_dir"])
+        rects = classifier(detector,
+                           grey,
+                           float(args["scale_factor"]),
+                           int(args["min_neighbors"]),
+                           int(args["cascade_width"]),
+                           int(args["cascade_height"]))
 
-    #
-    classifier_process(image, args["processed_images_dir"])
+        print("\n[INFO] Found " + str(classifier_output_count(rects)) + " companies house stamps.")
 
-#    cv2.waitKey(0)
+        #
+        classifier_output(rects, img, args["classifier_output_dir"])
+
+        #
+        classifier_process(img, join(args["processed_images_dir"], image))
+
+        #
+        if args["show_classifier_output"] == "True":
+            classifier_output_show(image, img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+        else:
+            pass
 
 if __name__ == "__main__":
 
@@ -120,4 +117,4 @@ if __name__ == "__main__":
     
     print("-"*50)
     print("Process Complete")
-    print("The time taken to process an image is : ", "{}".format((time.time() - process_start)/60, 2), " minutes")
+    print("The time taken to process an image is: ", "{}".format((time.time() - process_start)/60, 2), " minutes")
