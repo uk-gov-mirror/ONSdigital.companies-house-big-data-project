11e23fbbcabfa552f738d75edf9a434be7d8e361
Robert Stone <robert.stone@ext.ons.gov.uk>
2020-03-18 18:16:29 +0000

    can now call methods from module pipeline.

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,68 +1,15 @@
-# import the necessary packages
 from os import listdir
 from os.path import isfile, join
-import glob
+import time
 import argparse
 import sys
 import cv2
-import numpy
-import time
 
-def imgs_to_grey(path):
-    '''
-    '''
-    if path is None:
-        raise ValueError("Specify a path to an image.")
-    if type(path) not in [""]:
-        raise TypeError("The value of the path argument must be a of type string")
-    return cv2.cvtColor(path, cv2.COLOR_BGR2GRAY)
+import cst_data_processing
+import cst_classifier
+from cst_classifier import classifier_input, imgs_to_grey, classifier, classifier_output_count, classifier_output, classifier_process
 
-def classifier_input(path):
-    '''
-    '''
-    if path is None:
-        raise ValueError("Specify a path to an image.")
-    if type(path) not in [""]:
-        raise TypeError("The value of the path argument must be a of type string")
-    return cv2.imread(path)
-    
-def classifier(detector, imgs, scale_factor, min_neighbors, w, h):
-    '''
-    '''
-    return detector.detectMultiScale(imgs, scale_factor, min_neighbors, minSize = (w, h))
-
-def classifier_output_show(title, imgs):
-    '''
-    '''
-    return cv2.imshow(title, imgs)
-
-def classifier_output_count(imgs):
-    '''
-    '''
-    return len(imgs)
-
-def classifier_output(rects, image, path):
-    '''
-    '''
-    for (i, (x, y, w, h)) in enumerate(rects):
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        cv2.putText(image, "CH Stamp #{}".format(i + 1), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
-        roi_colour = image[y:y + h, x:x + w]
-        print("[INFO] Object classified. Saving locally")
-        cv2.imwrite(path + str(w) + str(h) + "_ch_stamps.png", roi_colour)
-        
-def classifier_process(image, path):
-    status = cv2.imwrite(path, image)
-    print("[INFO] Output from the classifier written to filesystem in: " + path, "[STATUS]: " + str(status))
-    
 def main():
-    '''
-
-    EXE on Mac " python3 cascade_classifier.py -d ../data/input/test_img_04.tif -c ../data/cascade_files/chp_stamp_cascade_classifier_20200224.xml -s 1.2 -m 10 -x 60 -y 30 -o ../data/output/classifier_output/ -p ../data/output/processed_images -v False/ "
-
-    EXE on a Windows machine " python cascade_classifier.py -d ..\data\input\test_img_04.tif -c ..\data\cascade_files\chp_stamp_cascade_classifier_20200224.xml -s 1.2 -m 10 -x 60 -y 30 -o ..\data\output\classifier_output\ -p ..\data\output\processed_images\ -v False"
-    
-    '''
     
     # construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser()
@@ -109,7 +56,7 @@ def main():
         classifier_process(img, join(args["processed_images_dir"], image))
 
         #
-        if args["show_classifier_output"] == "True":
+        if args["show_classifier_output"] == "True": 
             classifier_output_show(image, img)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
