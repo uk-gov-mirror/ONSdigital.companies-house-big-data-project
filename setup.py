8174eeb442d8d9e67d18595752b60b282c4ea232
Robert Stone <robert.stone@ext.ons.gov.uk>
2020-03-30 17:32:02 +0100

    The name of this project was changed to “ companies_house_accounts ”, and the distribution version number has been set to “ version 1.0.1 ”. This project is now set up to install several packages labelled "numpy", "scikit-image", "scikit-learn", and "opencv-python" locally within the companies house accounts repo. Two custom packages labelled “ src.data_processing.cst_data_processing ”and “ src.classifier.cst_classifier ” are also installed locally with this distribution. For now, the custom package labelled “ companies_house_accounts.tests ” is excluded from the distribution.

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name = "companies_house_accounts",
-    version = "1.0.0",
+    version = "1.0.1",
     description = "ons companies house project repos",
-    url = "http:",
+    url = "https://github.com/spudinator/companies_house_accounts.git",
     author = "Robert Stone",
     author_email = "robert.stone@ons.gov.uk",
     license = "MIT",
-    packages = find_packages()
+    packages = find_packages(exclude=["companies_house_accounts.tests"]),
+    install_requires = ["numpy", "scikit-image", "scikit-learn", "opencv-python"]
+    
+    
 )
