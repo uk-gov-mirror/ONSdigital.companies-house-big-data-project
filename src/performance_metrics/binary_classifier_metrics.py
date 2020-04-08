3d1c9fe8579de871658fe668e8925322075966ac
Robert Stone <robert.stone@ext.ons.gov.uk>
2020-04-08 15:27:54 +0100

    Added a class and empty methods to generate a confusion matrix, and then calculate the accuracy, precision, area under the curve metrics. From these metrics a report of the classifier performance will be generated.

--- src/performance_metrics/binary_classifier_metrics.py
+++ src/performance_metrics/binary_classifier_metrics.py
@@ -1,1 +1,27 @@
-'''
+class BinaryClassiferMetrics:
+    '''
+    '''
+    def binary_confusion_matrix():
+    '''
+    '''
+        pass
+        
+    def accuracy():
+    '''
+    '''
+        pass
+        
+    def precision():
+    '''
+    '''
+        pass
+    
+    def area_under_the_curve():
+    '''
+    '''
+        pass
+    
+    def metrics_reports():
+    '''
+    '''
+        pass
