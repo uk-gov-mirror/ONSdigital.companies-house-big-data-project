d7cf3ae72247f5ac47e0725f933deb4e6d9dda30
Kirsty228 <63724065+Kirsty228@users.noreply.github.com>
2020-04-16 13:19:07 +0100

    Update ch_xbrl_data.py
    Created the XbrlDataProcessing class and added the function unique_tag_count. The code is now ready for review.

--- src/data_processing/ch_xbrl_data.py
+++ src/data_processing/ch_xbrl_data.py
@@ -1,1 +1,31 @@
+import pyspark.sql.functions as F
 
+class XbrlDataProcessing:
+	'''
+	'''
+	def unique_tag_count(dataframe, name_column, crn_column, count_column):
+		"""
+		Groups by 'name' column to ascertain the values, 'tags'.
+		Counts of unique company records are summed and ordered by descending frequency
+		'distinct_count'.
+
+		Args:
+			dataframe:    a spark dataFrame
+			name_column:  column containing 'name' variable
+			crn_column:   column containing CRN variable
+			count_column: new column alias definition for frequency counts
+		Returns:
+			A dataframe ordered by new column, frequencies of distinct CRN values.
+		Raises:
+			None
+		"""
+		object = (
+			dataframe\
+			.groupBy(name_column)\
+			.agg(
+				F.countDistinct(crn_column).alias(count_column)
+			)\
+			.orderBy(F.col(count_column), ascending=False)
+		)
+
+		return (object)
