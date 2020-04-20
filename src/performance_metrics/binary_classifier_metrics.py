a3aa89b548af8b2907b363dbcafe8e130e7ab16a
elliottphillips-ONS <63713933+elliottphillips-ONS@users.noreply.github.com>
2020-04-20 15:14:46 +0100

    Update binary_classifier_metrics.py

--- src/performance_metrics/binary_classifier_metrics.py
+++ src/performance_metrics/binary_classifier_metrics.py
@@ -28,21 +28,8 @@ class BinaryClassiferMetrics:
         
     def precision():
     '''
-    Calculate the precision as a ratio of the number of true positives to the total number of positives.
-    
-    Arguments:
-        tp: number of true positives
-        fp: number of false positives  
-    
-    Raises:
-        None
-    
-    Returns:
-        Integer
-    
-    acc = tp / (tp + fp)
     '''
-        return tp / (tp + fp)
+        pass
     
     def area_under_the_curve():
     '''
