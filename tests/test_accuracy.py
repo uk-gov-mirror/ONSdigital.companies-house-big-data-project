23c28bf2eea27a5a5287686b629523689c887e97
Kirsty Cope <Kirsty.hopkins@ons.gov.uk>
2020-05-12 17:33:27 +0100

    Completed the test cases 'test_values' and 'test_types' for the accuracy function.

--- tests/test_accuracy.py
+++ tests/test_accuracy.py
@@ -10,34 +10,60 @@ class TestAccuracy(unittest.TestCase):
     """
     def test_accuracy_pos(self):
         """
-        positive test case for the accuracy function.
+        Positive test case for the accuracy function.
         """
         # Assume
         metrics = BinaryClassifierMetrics()
 
         # Assert
-        self.assertAlmostEqual(metrics.accuracy(tp = 1, tn = 1, fp = 1, fn = 1), 0.5)
+        self.assertAlmostEqual(metrics.accuracy(tp=1, tn=1, fp=1, fn=1), 0.5)
 
     def test_accuracy_neg(self):
         """
-        negative test case for the accuracy function.
+        Negative test case for the accuracy function.
         """
         # Assume
         metrics = BinaryClassifierMetrics()
 
         # Assert
-        self.assertNotEqual(metrics.accuracy(tp = 1, tn = 1, fp = 1, fn = 1), 0.8)
+        self.assertNotEqual(metrics.accuracy(tp=1, tn=1, fp=1, fn=1), 0.8)
 
     def test_types(self):
         """
+        Positive test case for the accuracy function.
         """
-        self.assertRaises()
+        # Assume
+        metrics = BinaryClassifierMetrics()
+
+        # Assert
+        with self.assertRaises(TypeError):
+            metrics.accuracy(1.0, 2, 3, 4)
+
+        with self.assertRaises(TypeError):
+            metrics.accuracy(1, 2.0, 3, 4)
+
+        with self.assertRaises(TypeError):
+            metrics.accuracy(1, 2, 3.0, 4)
+
+        with self.assertRaises(TypeError):
+            metrics.accuracy(1, 2, 3, 4.0)
 
     def test_values(self):
         """
+        Positive test case for the accuracy function.
         """
-        pass
+        # Assume
+        metrics = BinaryClassifierMetrics()
 
+        # Assert
+        with self.assertRaises(ValueError):
+            metrics.accuracy(None, 2, 3, 4)
 
-        
+        with self.assertRaises(ValueError):
+            metrics.accuracy(1, None, 3, 4)
 
+        with self.assertRaises(ValueError):
+            metrics.accuracy(1, 2, None, 4)
+
+        with self.assertRaises(ValueError):
+            metrics.accuracy(1, 2, 3, None)
