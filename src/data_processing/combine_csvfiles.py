6f970b7f8105a278c93b0dd92f58f01695ae19ef
Peter Derrick <peterd@meth-dsc-01.dsc.dmz>
2020-08-06 12:53:25 +0100

    cha_204/217: Added feature, csvs can now be merged on a quarterly basis. Input year is now a single value.

--- src/data_processing/combine_csvfiles.py
+++ src/data_processing/combine_csvfiles.py
@@ -3,12 +3,12 @@ import csv
 import itertools
 import pandas as pd
 from typing import List, Tuple
+from datetime import datetime
 
 def combine_csv(files: List[str], outfile: str):
     '''Combines all csv files listed in files into a single cs file using csv.writer.
     Unsuitable if files do not have the same format. Faster than the pandas version.'''
 
-    files.sort()
     print("Processing " + str(len(files)) + " files...")
     with open(outfile, "w") as w:
         writer = csv.writer(w, delimiter=',')
@@ -23,7 +23,6 @@ def combine_csv(files: List[str], outfile: str):
 def combine_csv_pd(files: List[str], outfile: str):
     '''Combines all csv files listed in files into a single csv file using pandas'''
 
-    files.sort()
     combined_csv = pd.concat([pd.read_csv(f, engine = 'python') for f in files])
     combined_csv.to_csv(outfile, index = False)
 
@@ -31,23 +30,50 @@ def _add_path(indir: str, files: str):
     '''returns list with paths appended to file names'''
     return [indir+i for i in files]
 
-def merge_files_by_year(indir: str, outdir: str, years: Tuple[int]):
+def merge_files_by_year(indir: str, outdir: str, year: int, quarter=None):
     ''' Finds all csv files with directory indir, groups files into
     individual years and calls combine_csv function to merge into
     single file given that the years falls within the range specified
     by the user.'''
 
+    # If the input directory is valid...
     if os.path.exists(indir):
 
+        # Create the output folder if it doesn't exist
         if not os.path.exists(outdir):
             os.mkdir(outdir)
 
+        # Get all csv files which contain the year specified in their filenames
         files = os.listdir(indir)
         files = ([i for i in files if i[-4:] == '.csv'
-                  and i[:4].isnumeric() and int(i[:4]) in range(*years)])
-        years = [[k,_add_path(indir, i)] for k, i in itertools.groupby(files, lambda x: x[:4])]
+                  and i[:4].isnumeric() and int(i[:4]) == year])
 
-        [combine_csv(files, outdir+year+'_xbrl.csv') for year, files in years]
+        # Sort all files by the month contained in the file name
+        files = sorted(files, key=lambda day: datetime.strptime(day.split("-")[1].split("_")[0], "%B"))
+
+        # Create a list of months based on what quarter in the year has been specified
+        if quarter == 1:
+            quarter_list = ['January', 'February', 'March']
+        elif quarter == 2:
+            quarter_list = ['April', 'May', 'June']
+        elif quarter == 3:
+            quarter_list = ['July', 'August', 'September']
+        elif quarter == 4:
+            quarter_list = ['October', 'November', 'December']
+        else:
+            quarter_list = []
+
+        # Filter out those files not in the specified quarter,
+        # if a quarter has been specified
+        if quarter != None:
+            files = [f for f in files if f.split("-")[1].split("_")[0] in quarter_list]
+
+        # Prepend input directory to filenames
+        files = [indir + f for f in files]
+
+        # Combine the csvs
+        combine_csv(files, outdir + str(year) + '_xbrl.csv')
+
     else:
         print("Input file path does not exist")
 
@@ -55,6 +81,7 @@ if __name__ == '__main__':
 
     indir = '/shares/data/20200519_companies_house_accounts/xbrl_parsed_data/'
     outdir = '/home/peterd/test/'
-    years = (2008, 2011)
+    year = 2010
+    quarter = 1
 
-    merge_files_by_year(indir, outdir, years)
+    merge_files_by_year(indir, outdir, year, quarter)
