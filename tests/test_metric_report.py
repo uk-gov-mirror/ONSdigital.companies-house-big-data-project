9700edb22273e5f8cb30d3e15110b1f6d37497bd
Robert Stone <robert.stone@ext.ons.gov.uk>
2020-08-31 22:56:58 +0100

    Ran code for test_metric_report.py

--- tests/test_metric_report.py
+++ tests/test_metric_report.py
@@ -1,7 +1,7 @@
 import unittest
 
-from metrics.metric_report import PipelineMetrics
-
+# Custom import
+from src.performance_metrics.binary_classifier_metrics import BinaryClassifierMetrics
 
 class TestMetricReport(unittest.TestCase):
     """
@@ -14,34 +14,34 @@ class TestMetricReport(unittest.TestCase):
         passed are of type float less than or equal to 1.
         """
         # Assume
-        metrics = PipelineMetrics()
+        metrics = BinaryClassifierMetrics()
 
         # Assert
         with self.assertRaises(TypeError):
-            metrics.metrics_reports(0.1, 0.2, 0.3, 0.4)
+            metrics.metrics_report(0.1, 0.2, 0.3, 0.4)
 
         with self.assertRaises(TypeError):
-            metrics.metrics_reports(0.1, 0.2, 0.3, 0.4)
+            metrics.metrics_report(0.1, 0.2, 0.3, 0.4)
 
         with self.assertRaises(TypeError):
-            metrics.metrics_reports(0.1, 0.2, 0.3, 0.4)
+            metrics.metrics_report(0.1, 0.2, 0.3, 0.4)
 
         with self.assertRaises(TypeError):
-            metrics.metrics_reports(0.1, 0.2, 0.3, 0.4)
+            metrics.metrics_report(0.1, 0.2, 0.3, 0.4)
 
     def test_precision_negative(self):
         """
         Negative test case for the metric_report function.
         """
         # Assume
-        metrics = PipelineMetrics()
+        metrics = BinaryClassifierMetrics()
 
         # Assert
         with self.assertRaises(TypeError):
-            metrics.metrics_reports(10, 0.2, 0.3, 0.4)
+            metrics.metrics_report(10, 0.2, 0.3, 0.4)
 
         with self.assertRaises(TypeError):
-            metrics.metrics_reports(0.1, 20, 0.3, 0.4)
+            metrics.metrics_report(0.1, 20, 0.3, 0.4)
 
         with self.assertRaises(TypeError):
             metrics.metrics_reports(0.1, 0.2, 30, 0.4)
@@ -54,41 +54,37 @@ class TestMetricReport(unittest.TestCase):
         Test correct argument types passed.
         """
         # Assume
-        metrics = PipelineMetrics()
+        metrics = BinaryClassifierMetrics()
 
         # Assert
         with self.assertRaises(TypeError):
-            metrics.metrics_reports(1, 1.0, 1.0, 1.0)
+            metrics.metrics_report(1, 1.0, 1.0, 1.0)
 
         with self.assertRaises(TypeError):
-            metrics.metrics_reports(1.0, 1, 1.0, 1.0)
+            metrics.metrics_report(1.0, 1, 1.0, 1.0)
 
         with self.assertRaises(TypeError):
-            metrics.metrics_reports(1.0, 1.0, 1, 1.0)
+            metrics.metrics_report(1.0, 1.0, 1, 1.0)
 
         with self.assertRaises(TypeError):
-            metrics.metrics_reports(1.0, 1.0, 1.0, 1)
+            metrics.metrics_report(1.0, 1.0, 1.0, 1)
 
     def test_values(self):
         """
         Test values are provided as arguments.
         """
         # Assume
-        metrics = PipelineMetrics()
+        metrics = BinaryClassifierMetrics()
 
         # Assert
         with self.assertRaises(ValueError):
-            metrics.metrics_reports(None, 0.2, 0.3, 0.4)
+            metrics.metrics_report(None, 0.2, 0.3, 0.4)
 
         with self.assertRaises(ValueError):
-            metrics.metrics_reports(0.1, None, 0.3, 0.4)
+            metrics.metrics_report(0.1, None, 0.3, 0.4)
 
         with self.assertRaises(ValueError):
-            metrics.metrics_reports(0.1, 0.2, None, 0.4)
+            metrics.metrics_report(0.1, 0.2, None, 0.4)
 
         with self.assertRaises(ValueError):
-            metrics.metrics_reports(0.1, 0.2, 0.3, None)
-
-
-if __name__ == '__main__':
-    unittest.main()
+            metrics.metrics_report(0.1, 0.2, 0.3, None)
