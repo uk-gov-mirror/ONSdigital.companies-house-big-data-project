a84a242e981e348c33b21a2c6d70cb76eaf7c5b5
Kirsty Cope <kirstyc@meth-dsc-01.dsc.dmz>
2020-09-09 17:19:01 +0100

    Updated the script to combine and read in the list_of_tags.txt for each year and produce an xlsx with the unique list of tags for that year (2011 data produced) along with their frequency.

--- src/data_processing/CHA_276_name_and_freq_of_xbrl_tags.py
+++ src/data_processing/CHA_276_name_and_freq_of_xbrl_tags.py
@@ -1,9 +1,104 @@
+import os
+import csv
+import itertools
 import pandas as pd
+from typing import List, Tuple
+from datetime import datetime
 
+"""
 # The test data to call in
-data_2011 = pd.read_csv("/home/kirstyc/test/2011_xbrl.csv", nrows=200)
-data_2012 = pd.read_csv("/home/kirstyc/test/2012_xbrl.csv", nrows=200)
-data_2013 = pd.read_csv("/home/kirstyc/test/2013_xbrl.csv", nrows=200)
+data_2010 = pd.read_csv("/home/kirstyc/test/2011_xbrl.csv", nrows=200)
+data_2011 = pd.read_csv("/home/kirstyc/test/2012_xbrl.csv", nrows=200)
+#data_2013 = pd.read_csv("/home/kirstyc/test/2013_xbrl.csv", nrows=200)
+"""
+
+def combine_csv(files: List[str], outfile: str):
+    '''Combines all csv files listed in files into a single cs file using csv.writer.
+    Unsuitable if files do not have the same format. Faster than the pandas version.'''
+
+    print("Processing " + str(len(files)) + " files...")
+    with open(outfile, "w") as w:
+        writer = csv.writer(w, delimiter=',')
+        for n, file in enumerate(files):
+            print("Processing " + file + "...")
+            with open(file, "r") as f:
+                reader = csv.reader(f, delimiter=',')
+                if n > 0: next(reader)
+                for row in reader:
+                 writer.writerow(row)
+
+def combine_csv_pd(files: List[str], outfile: str):
+    '''Combines all csv files listed in files into a single csv file using pandas'''
+
+    combined_csv = pd.concat([pd.read_csv(f, engine = 'python') for f in files])
+    combined_csv.to_csv(outfile, index = False)
+
+def _add_path(indir: str, files: str):
+    '''returns list with paths appended to file names'''
+    return [indir+i for i in files]
+
+def merge_files_by_year(indir: str, outdir: str, year: int, quarter=None):
+    ''' Finds all csv files with directory indir, groups files into
+    individual years and calls combine_csv function to merge into
+    single file given that the years falls within the range specified
+    by the user.'''
+
+    # If the input directory is valid...
+    if os.path.exists(indir):
+
+        # Create the output folder if it doesn't exist
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+
+        # Get all csv files which contain the year specified in their filenames
+        files = os.listdir(indir)
+        files = ([i for i in files if i[-4:] == '.txt'
+                  and i[:4].isnumeric() and int(i[:4]) == year])
+
+        # Sort all files by the month contained in the file name
+        files = sorted(files, key=lambda day: datetime.strptime(day.split("-")[1].split("_")[0], "%B"))
+
+        # Create a list of months based on what quarter in the year has been specified
+        if quarter == 1:
+            quarter_list = ['January', 'February', 'March']
+        elif quarter == 2:
+            quarter_list = ['April', 'May', 'June']
+        elif quarter == 3:
+            quarter_list = ['July', 'August', 'September']
+        elif quarter == 4:
+            quarter_list = ['October', 'November', 'December']
+        else:
+            quarter_list = []
+
+        # Filter out those files not in the specified quarter,
+        # if a quarter has been specified
+        if quarter != None:
+            files = [f for f in files if f.split("-")[1].split("_")[0] in quarter_list]
+            quarter_str = "q" + str(quarter)
+        else:
+            quarter_str = ""
+
+        # Prepend input directory to filenames
+        files = [indir + f for f in files]
+
+        # Combine the csvs
+        combine_csv(files, outdir + str(year) + quarter_str + 'annual_list_of_tags_xbrl.csv')
+
+    else:
+        print("Input file path does not exist")
+
+if __name__ == '__main__':
+
+    indir = '/shares/data/20200519_companies_house_accounts/logs/'
+    outdir = '/home/kirstyc/test0909/'
+    year = 2011
+    quarter = None
+
+    merge_files_by_year(indir, outdir, year, quarter)
+
+import pandas as pd
+test = pd.read_csv('/home/kirstyc/test0909/2011_xbrl.csv', header=None, names=['tag_name'])
+
 
 
 # for CHA_276 - get the tag names and frequencies for each year and create a spreadsheet with a worksheet/tab for each
@@ -29,9 +124,8 @@ def tags_and_frequencies(df, col_name):
 
     return df_tags_and_counts
 
-annual_2011 = tags_and_frequencies(data_2011, 'name')
-annual_2012 = tags_and_frequencies(data_2012, 'name')
-annual_2013 = tags_and_frequencies(data_2013, 'name')
+annual_2011 = tags_and_frequencies(test, 'tag_name')
+
 
 # Writes to an Excel workbook
 def write_to_excel(input_data, directory, filename, sheet_name, create_new_workbook = True):
@@ -64,10 +158,13 @@ def write_to_excel(input_data, directory, filename, sheet_name, create_new_workb
     return output
 
 
-test = write_to_excel(annual_2013, '/home/kirstyc/test/', 'test_again2', '2013', create_new_workbook = True)
+test = write_to_excel(annual_2011, '/home/kirstyc/repos/companies_house_accounts/data/', 'cha_276_2011_list_of_tags', '2011', create_new_workbook = True)
+
+test2 = pd.read_excel('/home/kirstyc/test0909/2011_list_of_tags.xlsx', index_col=0)
+print(test2)
 
 # Cannot view the xlsx in VM so reading the workbook 'test_cha_276.xlsx' into here to check it, one worksheet at a time
-data2013 = pd.read_excel('/home/kirstyc/test/test_cha_276.xlsx', sheet_name=['data_2013'])
+#data2013 = pd.read_excel('/home/kirstyc/test/test_cha_276.xlsx', sheet_name=['data_2013'])
 
 
 # NEXT STEP:
