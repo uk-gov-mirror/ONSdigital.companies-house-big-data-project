b85b07b2439cf60864cdcb1bbf0617d409c209af
lewyse-lee <lewyse.lee@ons.gov.uk>
2020-10-07 15:27:05 +0000

    REfactored the function retrieve_list_of_tags in the xbrl_pd_methods script to now include value and type errors. Additionally created a unit test for the retrive_list_of_tags function - saved in the tests folder. This contains a test on the file name that is exported and that the open function is called, a type and a value unit test.

--- tests/test_retrieve_list_of_tags.py
+++ tests/test_retrieve_list_of_tags.py
@@ -1,189 +1,108 @@
 import unittest
-<<<<<<< HEAD
 import pandas
 import unittest.mock as mock
-=======
 import pandas as pd
 from pandas.testing import assert_frame_equal
 from unittest.mock import Mock
 from unittest.mock import patch
->>>>>>> 3c95e861b9f940374cfd5b50861fc30f58fb14d3
 
 # Custom import
 from src.data_processing.xbrl_pd_methods import XbrlExtraction
 
-<<<<<<< HEAD
 class RetrieveListOfTags(unittest.TestCase):
+
     def input_data(self):
+        '''
 
-        df = pandas.DataFrame([['A', '10'],
-=======
-
-class TestTagExtraction(unittest.TestCase):
-    """
-
-    """
-    #@patch("home/lewysel/repos/companies_house_accounts/src/xbrl_pd_methods.py.list_of_tags_unique")
-
-    def test_retrieve_list_of_tags(self, list_mock):
-        """
-        Positive test case for the retrieve_list_of_tags function.
-        Which tests for the correct list being produced
-        """
-
+        :return:
+        '''
         # Dataframe that we create.
         df = pd.DataFrame([['A', '10'],
->>>>>>> 3c95e861b9f940374cfd5b50861fc30f58fb14d3
-                            ['B', '20'],
-                            ['A', '30'],
-                            ['C', '40'],
-                            ['D', '50'],
-                            ['C', '60']],
-<<<<<<< HEAD
-                           columns=['name', 'Age'])
-
+                           ['B', '20'],
+                           ['A', '30'],
+                           ['C', '40'],
+                           ['D', '50'],
+                           ['C', '60']],
+                          columns=['name', 'Age'])
         return df
-    '''
-    
-    '''
+
     @mock.patch('builtins.open')
     def test_retrieve_list_of_tags_pos(self,mock_open):
         '''
-        Test to check if the open function is called
+        Test to check if the open function is called and that the file name and directory
+        is as expected. Additionally checks the file is in write mode.
 
-        :param mock_open:
+        :param mock_open: mocked instance of the builtin python open method
 
-        :return:
+        :return: None
 
         '''
+
         #define test variables
         df = self.input_data()
         column = 'name'
+        folder = 'test_folder'
+        month = 'January'
+        year = '2010'
 
-
-        mock_file_name = 'content_folder/2010-January_list_of_tags.txt'
+        mock_file_name = folder+'/'+year+'-'+month+'_list_of_tags.txt'
 
         with mock.patch('builtins.open',mock_open()) as mocked_file:
-            XbrlExtraction().retrieve_list_of_tags(df,'name','content_folder','January','2010')
+            XbrlExtraction().retrieve_list_of_tags(df,column,folder,month,year)
             mocked_file.assert_called_once_with(mock_file_name,'w')
 
+    def test_retrieve_list_of_tags_type(self):
+        '''
 
-        #extractor.retrieve_list_of_tags(df,column, "output_folder", "January", "2010")
+        :return:
+        '''
+        extractor = XbrlExtraction()
+        df = self.input_data()
 
-        #check if open has been called
-        #self.assertTrue(mock_open.called,"Text file not outputted")
+        #check if dataframe = string
+        with self.assertRaises(TypeError):
+            extractor.retrieve_list_of_tags('df', 'name', 'output_folder', 'January', '2010')
 
+        #check type error for column = list type
+        with self.assertRaises(TypeError):
+            extractor.retrieve_list_of_tags(df,['name'],'output_folder','January','2010')
 
+        #check type error for folder = list
+        with self.assertRaises(TypeError):
+            extractor.retrieve_list_of_tags(df, 'name', ['output_folder'], 'January', '2010')
 
+        #check type error for month = int
+        with self.assertRaises(TypeError):
+            extractor.retrieve_list_of_tags(df, 'name', 'output_folder', 1, '2010')
 
+        # check type error for year = int
+        with self.assertRaises(TypeError):
+            extractor.retrieve_list_of_tags(df, 'name', 'output_folder', 'January', 2010)
+
+    def test_retrieve_list_of_tags_value(self):
+        '''
+
+        :return:
+        '''
+        extractor = XbrlExtraction()
+        df = self.input_data()
+
+        # check if column not in dataframe raises error
+        with self.assertRaises(ValueError):
+            extractor.retrieve_list_of_tags(df, 'potatoe', 'output_folder', 'January', '2010')
+
+        # check value error for invalid directory
+        with self.assertRaises(ValueError):
+            extractor.retrieve_list_of_tags(df, 'name', 'not_real_directory', 'January', '2010')
+
+        # check value error for invalid month
+        with self.assertRaises(ValueError):
+            extractor.retrieve_list_of_tags(df, 'name', 'output_folder', 'Jan', '2010')
+
+        # check value error if year not also int
+        with self.assertRaises(ValueError):
+            extractor.retrieve_list_of_tags(df, 'name', 'output_folder', 'January', '2k10')
+
+#Is this bit needed?
 if __name__ == '__main__':
     unittest.main()
-=======
-                           columns=['name', 'value'])
-
-
-        # The list the function should produce
-        expected_list = ['A','B','C','D']
-
-
-        # Assume
-        subsets = XbrlExtraction()
-
-        # Assume 1
-        tp_tag_extraction1 = subsets.retrieve_list_of_tags(df, 'Nhome/lewysel/repos/companies_house_accounts/tests/test_outputs/retrieve_list_of_tags', 'Month','Year')
-
-        # Assert 1
-        assert_frame_equal(tp_tag_extraction1.reset_index(drop=True),
-                           df2.reset_index(drop=True))
-        # Assert 2
-        assert_frame_equal(tp_tag_extraction2.reset_index(drop=True),
-                           df3.reset_index(drop=True))
-'''
-    def test_tag_extraction_neg(self):
-        """
-        Negative test case for the tag_extraction function.
-        """
-
-        # Dataframe that we create.
-        df1 = pd.DataFrame([['A', '10'],
-                            ['B', '20'],
-                            ['A', '30'],
-                            ['C', '40'],
-                            ['D', '50'],
-                            ['C', '60']],
-                           columns=['Name', 'Age'])
-
-        # Dataframe that is NOT the same as the one the function should return.
-        df2 = pd.DataFrame([['10'],
-                            ['30']],
-                           columns=['Age'])
-
-        # Dataframe that is NOT the same as the one the function should return.
-        df3 = pd.DataFrame([['A', '10'],
-                            ['C', '40'],
-                            ['A', '30'],
-                            ['C', '60']],
-                           columns=['Name', 'Age'])
-
-        # Assume
-        subsets = XbrlSubsets()
-
-        # Assume 1
-        tn_tag_extraction1 = subsets.tag_extraction(df1, 'Name', 'A')
-        # Assume 2
-        tn_tag_extraction2 = subsets.tag_extraction(df1, 'Name', ['A', 'C'])
-
-        # Assert 1
-        self.assertNotEqual(tn_tag_extraction1.reset_index(drop=True).equals(df2.reset_index(drop=True)), True)
-        # Assert 2
-        self.assertNotEqual(tn_tag_extraction2.reset_index(drop=True).equals(df2.reset_index(drop=True)), True)
-
-    def test_types(self):
-        """
-        Positive test case for the tag_extraction function.
-        """
-        # Assume
-        df1 = pd.DataFrame([['A', '10'],
-                            ['B', '20'],
-                            ['A', '30'],
-                            ['C', '40'],
-                            ['D', '50']],
-                           columns=['Name', 'Age'])
-        # Assume
-        subsets = XbrlSubsets()
-
-        # Assert
-        with self.assertRaises(TypeError):
-            subsets.tag_extraction(1.0, 'Name', 'A')
-
-        with self.assertRaises(TypeError):
-            subsets.tag_extraction(df1, 1, 'A')
-
-        with self.assertRaises(TypeError):
-            subsets.tag_extraction(df1, 'Name', {'A': 'a'})
-
-        with self.assertRaises(TypeError):
-            subsets.tag_extraction(df1, 'Name', all)
-
-    def test_values(self):
-        """
-        Positive test case for the tag_extraction function.
-        """
-        # Assume
-        df1 = pd.DataFrame([['A', '10'],
-                            ['B', '20'],
-                            ['A', '30'],
-                            ['C', '40'],
-                            ['D', '50']],
-                           columns=['Name', 'Age'])
-        # Assume
-        subsets = XbrlSubsets()
-
-        # Assert
-        with self.assertRaises(ValueError):
-            subsets.tag_extraction(df1, 'names', 'A')
-
-        with self.assertRaises(ValueError):
-            subsets.tag_extraction(df1, 'A', 'Name')
-'''
->>>>>>> 3c95e861b9f940374cfd5b50861fc30f58fb14d3
