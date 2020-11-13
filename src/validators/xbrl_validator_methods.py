8335b603781f9cd7bd398af32037bb2e10f4354d
Dylan Purches <dylan.purches@ext.ons.gov.uk>
2020-11-13 16:29:59 +0000

    Reformatted to ensure common coding standards

--- src/validators/xbrl_validator_methods.py
+++ src/validators/xbrl_validator_methods.py
@@ -31,7 +31,9 @@ class XbrlValidatorMethods:
                     print("File size: " + str(getsize(file)) + " bytes")
 
                     file_time = datetime.utcfromtimestamp(getmtime(file))
-                    print("File modified: " + file_time.strftime("%Y-%m-%d %H:%M:%S.%f+00:00 (UTC)"))
+                    print("File modified: "
+                          + file_time.strftime("%Y-%m-%d \
+                          %H:%M:%S.%f+00:00 (UTC)"))
             else:
                 print("Specified filepath is not a directory!")
 
@@ -39,14 +41,17 @@ class XbrlValidatorMethods:
             print("Specified directory does not exist!")
 
     @staticmethod
-    def validate_extracted_files(filepath_to_zip_files, filepath_to_extracted_files):
+    def validate_extracted_files(filepath_to_zip_files,
+                                 filepath_to_extracted_files):
         """
         Validates and returns information on files in specified directory
-        To be used for validating files which have been extracted from a zip file
+        To be used for validating files which have been extracted from a
+        zip file.
 
         Arguments:
-            filepath_to_zip_files: source directory of files of interest
-            filepath_to_extracted_files: destination directory of files of interest
+            filepath_to_zip_files:        source directory of files of interest
+            filepath_to_extracted_files:  destination directory of files of
+                                          interest
         Returns:
             None
         Raises:
@@ -55,12 +60,14 @@ class XbrlValidatorMethods:
 
         def get_size_of_folder(start_path):
             """
-            Recursive method to return the combined size of a folder and its sub-directories
+            Recursive method to return the combined size of a folder and its
+            sub-directories
 
             Arguments:
                 start_path: directory to be checked
             Returns:
-                total_size: combined size of the directory and its sub-directories
+                total_size: combined size of the directory and its
+                sub-directories
             Raises:
                 None
             """
@@ -74,11 +81,14 @@ class XbrlValidatorMethods:
             return total_size
 
         # If both filepaths to the zip files and the extracted files exist...
-        if exists(filepath_to_zip_files) and exists(filepath_to_extracted_files):
+        if exists(filepath_to_zip_files) \
+                and exists(filepath_to_extracted_files):
 
-            zip_files = [filepath_to_zip_files + "/" + f for f in listdir(filepath_to_zip_files)]
+            zip_files = [filepath_to_zip_files + "/" + f
+                         for f in listdir(filepath_to_zip_files)]
             extracted_files = [filepath_to_extracted_files + "/" + f
-                               for f in listdir(filepath_to_extracted_files) if not isfile(f)]
+                               for f in listdir(filepath_to_extracted_files)
+                               if not isfile(f)]
 
             zip_files.sort()
             extracted_files.sort()
@@ -93,11 +103,14 @@ class XbrlValidatorMethods:
                 extracted_folder_size = get_size_of_folder(e)
 
                 print("Zip file size: " + str(zip_size) + " bytes")
-                print("Extracted folder size: " + str(extracted_folder_size) + " bytes")
+                print("Extracted folder size: " + str(extracted_folder_size)
+                      + " bytes")
                 if extracted_folder_size >= zip_size:
-                    print("Extracted folder size larger than original zip file - size check OK")
+                    print("Extracted folder size larger than original zip file\
+                    - size check OK")
                 else:
-                    print("Extracted folder size smaller than original zip file - size check failed!")
+                    print("Extracted folder size smaller than original \
+                    zip file - size check failed!")
 
                 # Check to see if all files have been extracted correctly
                 files_in_zip = set(zipfile.ZipFile(z).namelist())
