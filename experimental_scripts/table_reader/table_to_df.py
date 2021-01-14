cff70abb451dd83a86934bd1b4380f3a93842fab
Dylan Purches <dylan.purches@ext.ons.gov.uk>
2021-01-14 17:23:40 +0000

    Added to table_to_df function to begin to create function to assign dates and units to column headers

--- experimental_scripts/table_reader/table_to_df.py
+++ experimental_scripts/table_reader/table_to_df.py
@@ -1,5 +1,6 @@
 import gcsfs
 import pandas as pd
+import regex
 
 class Table2Df:
 
@@ -8,13 +9,13 @@ class Table2Df:
         self.fs = fs
         self.df = None
         self.data = table_fit.data
+        self.table_data = self.table.data.drop(self.table.header_indices)
 
     def reconstruct_table(self):
         headers = self.headers_to_strings(self.table.header_groups)
-        table_data = self.table.data.drop(self.table.header_indices)
 
         new_df = pd.DataFrame(columns=headers)
-        for index, row in table_data.iterrows():
+        for index, row in self.table_data.iterrows():
             new_df.loc[row["line_num"], headers[int(row["column"])]] = row["value"]
         return new_df
 
@@ -30,5 +31,29 @@ class Table2Df:
             str_headers.append(new_string)
         return str_headers
 
+    def get_info_headers(self, years = range(1999,2020)):
+        currency_indexes = [i for i in self.table.header_indices if
+                            len(regex.findall(r"\p{Sc}", self.data.loc[i, "value"]))]
+        self.unit_headers = currency_indexes
+        
+        date_indexes = []
+        for i in self.table.header_indices:
+            print([(str(y) in self.data.loc[i, "value"]) for y in years])
+            contains_year = any([str(y) in self.data.loc[i, "value"] for y in years])
+            if contains_year:
+                date_indexes.append(i)
+        self.date_headers = date_indexes
 
+        header_data = pd.DataFrame(columns=["columns", "dates", "units"])
+        relevant_cols = []
+        for i, g in enumerate(self.table.header_groups):
+            unit_col = [j for j in g if j in self.unit_headers]
+            date_col = [j for j in g if j in self.date_headers]
+            if len(unit_col)==1  or len(date_col)==1:
+                relevant_cols.append(i+1)
+                if not unit_col:
+                    date = self.data.loc[date_col[0], "value"]
+                    unit = 0
+                elif not date_col:
+                    pass
 
