526dde093746483475a6dd5e664015f0731ed3ea
Dylan Purches <dylan.purches@ext.ons.gov.uk>
2021-01-15 12:30:06 +0000

    Added to table_to_df module so that tables similar to xbrl data are produced

--- experimental_scripts/table_reader/table_to_df.py
+++ experimental_scripts/table_reader/table_to_df.py
@@ -38,22 +38,48 @@ class Table2Df:
         
         date_indexes = []
         for i in self.table.header_indices:
-            print([(str(y) in self.data.loc[i, "value"]) for y in years])
             contains_year = any([str(y) in self.data.loc[i, "value"] for y in years])
             if contains_year:
                 date_indexes.append(i)
         self.date_headers = date_indexes
 
-        header_data = pd.DataFrame(columns=["columns", "dates", "units"])
-        relevant_cols = []
+        header_data = pd.DataFrame(columns=["column", "date", "unit"])
         for i, g in enumerate(self.table.header_groups):
             unit_col = [j for j in g if j in self.unit_headers]
             date_col = [j for j in g if j in self.date_headers]
             if len(unit_col)==1  or len(date_col)==1:
-                relevant_cols.append(i+1)
+                header_data.loc[i, "column"] = i+1
                 if not unit_col:
-                    date = self.data.loc[date_col[0], "value"]
-                    unit = 0
+                    header_data.loc[i, "date"] = self.data.loc[date_col[0], "value"]
+                    header_data.loc[i, "unit"] = self.get_closest_el(self.data, date_col[0], self.unit_headers)
                 elif not date_col:
-                    pass
+                    header_data.loc[i, "date"] = self.get_closest_el(self.data, unit_col[0], self.date_headers)
+                    header_data.loc[i, "unit"] = self.data.loc[unit_col[0], "value"]
+                else:
+                    header_data.loc[i, "date"] = self.data.loc[date_col[0], "value"]
+                    header_data.loc[i, "unit"] = self.data.loc[unit_col[0], "value"]
+        return header_data
+
+    def get_final_df(self):
+        self.df = self.table_data.merge(self.get_info_headers(), on="column")
+        for index, row in self.df.iterrows():
+            l = row["line_num"]
+            try:            
+                # print(self.df[(self.df["line_num"]==l)&(self.df["column"]==0)].iloc[0]["value"])
+                self.df.loc[index, "name"] = self.data[(self.data["line_num"]==l)&(self.data["column"]==0)].iloc[0]["value"]
+            except:
+                self.df.loc[index, "name"] = None
+        self.df = self.df[["name", "value", "date", "unit"]]
+
+    @staticmethod
+    def get_central_coord(data, i):
+        return 0.5*(eval(data.loc[i, "normed_vertices"])[3][0] + eval(data.loc[i, "normed_vertices"])[2][0])
+    
+    @staticmethod
+    def get_closest_el(data, x, elements):
+        dists = []
+        for el in elements:
+            dists.append(abs(Table2Df.get_central_coord(data, el) - Table2Df.get_central_coord(data, x)))
+        return data.loc[elements[dists.index(min(dists))], "value"]
+        
 
