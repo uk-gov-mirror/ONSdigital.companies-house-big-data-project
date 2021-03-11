6fbae9a1ced68d157d65a6a089668ef6c1b21690
PeterJDerrick89 <peter.derrick@ons.gov.uk>
2021-03-11 13:57:07 +0000

    Minor tweaks to bash script for demo purposes

--- archive/src/method_finder.sh
+++ archive/src/method_finder.sh
@@ -1,14 +1,14 @@
-#echo "===self.method() calls==="
-#self_method_list=$(grep -r --include \*.py -E 'self\.[a-zA-Z0-9_]+\(' | grep -Ev 'self\.fs|self\.__init__')
-#echo "$self_method_list"
+echo "===self.method() calls==="
+self_method_list=$(grep -r --include \*.py -E 'self\.[a-zA-Z0-9_\.]+\(' | grep -Ev 'self\.fs|self\.__init__')
+echo "$self_method_list"
 
-#echo "===Method Headers==="
-#method_headers=$(grep -r --include \*.py 'def ')
-#echo "$method_headers"
+echo "===Method Headers==="
+method_headers=$(grep -r --include \*.py 'def ')
+echo "$method_headers"
 
-#echo "===Classes==="
-#class_headers=$(grep -r --include \*.py -E 'class ' | grep -Ev '"""' | grep -E ':$')
-#echo "$class_headers"
+echo "===Classes==="
+class_headers=$(grep -r --include \*.py -E 'class ' | grep -Ev '"""' | grep -E ':$')
+echo "$class_headers"
 
 echo "===Class names==="
 class_names=$(grep -r --include \*.py -E 'class ' | grep -Ev '"""' | grep -E ':$' | awk '{print $2}' | awk -F '(' '{print $1}' | sed 's/://g')
@@ -18,13 +18,13 @@ class_names=($class_names)
 
 # ====================================
 
-# echo "===Class static method calls==="
-# for c in ${class_names[@]}; do
+echo "===Class static method calls==="
+for c in ${class_names[@]}; do
 
-#     echo '---'$c'---'
-#     instances=$(grep -rh --include \*.py -E "$c"'\.[a-zA-Z0-9_\.]+\(' | sed 's/^[ \t]*//g' | grep -Ev 'import|^#' | awk -F " = " '{if (NF==1) print $1; else print $2;}')
-#     echo "$instances"
-# done
+    echo '---'$c'---'
+    instances=$(grep -rh --include \*.py -E "$c"'\.[a-zA-Z0-9_\.]+\(' | sed 's/^[ \t]*//g' | grep -Ev 'import|^#' | awk -F " = " '{if (NF==1) print $1; else print $2;}')
+    echo "$instances"
+done
 
 # ================================
 
