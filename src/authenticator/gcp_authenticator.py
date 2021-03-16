c13f96fe2081ae0631993652a207050963fb4921
Dylan Purches <dylan.purches@ext.ons.gov.uk>
2021-03-16 12:53:16 +0000

    Added functions to delete keys, and add keys if they are not in a directory

--- src/authenticator/gcp_authenticator.py
+++ src/authenticator/gcp_authenticator.py
@@ -8,13 +8,18 @@ import shutil
 
 class GCPAuthenticator:
     """Class for downloading keys relevant to pipeline modules"""
-    def __init__(self):
+    def __init__(self, iam_access_sa, project):
+        
+        self.iam_access_sa = iam_access_sa
+        self.project = project
+
         self.master_key = None
         self.xbrl_scraper_key = None
         self.xbrl_unpacker_key = None
+        self.xbrl_validator_key = None
         self.xbrl_parser_key = None
 
-    def get_sa_keys(self, keys_filepath, project, get_keys_sa):
+    def get_sa_keys(self, keys_filepath):
         
         if not os.path.isdir(keys_filepath):
             raise ValueError(
@@ -25,48 +30,73 @@ class GCPAuthenticator:
         "SECURITY WARNING - do not save keys within a git repo! Ensure folder is immediately deleted from repo"
         )   
 
-        keys_to_download = []
         if os.path.exists(keys_filepath + "/master_key.json"):
-            master_fp = self.read_json(keys_filepath + "/master_key.json")
-            self.master_key = master_fp
-            self.xbrl_scraper_key = master_fp
-            self.xbrl_unpacker_key = master_fp
-            self.xbrl_parser_key = master_fp
+            master_key = self.read_json(keys_filepath + "/master_key.json")
+            self.master_key = master_key
+            self.xbrl_scraper_key = master_key
+            self.xbrl_unpacker_key = master_key
+            self.xbrl_validator_key = master_key
+            self.xbrl_parser_key = master_key
+
         else:
             if os.path.exists(keys_filepath + "/xbrl_scraper_key.json"):
                 self.xbrl_scraper_key = self.read_json(keys_filepath + "/xbrl_scraper_key.json")
             else:
-                keys_to_download.append("xbrl_scraper_key")
+                print("Downloading xbrl scraper key")
+                self.xbrl_scraper_key = self.create_key("xbrl-web-scraper@ons-companies-house-dev.iam.gserviceaccount.com")
 
             if os.path.exists(keys_filepath + "/xbrl_unpacker_key.json"):
                 self.xbrl_unpacker_key = self.read_json(keys_filepath + "/xbrl_unpacker_key.json")
             else:
-                keys_to_download.append("xbrl_unpacker_key")
+                self.xbrl_unpacker_key = self.create_key("xbrl-unpacker@ons-companies-house-dev.iam.gserviceaccount.com")
+
+            if os.path.exists(keys_filepath + "/xbrl_validator_key.json"):
+                self.xbrl_validator_key = self.read_json(keys_filepath + "/xbrl_validator_key.json")
+            else:
+                self.xbrl_validator_key = self.create_key("xbrl-validator@ons-companies-house-dev.iam.gserviceaccount.com")
 
             if os.path.exists(keys_filepath + "/xbrl_parser_key.json"):
                 self.xbrl_parser_key = self.read_json(keys_filepath + "/xbrl_parser_key.json")
             else:
-                keys_to_download.append("xbrl_unpacker_key")
-        
+                self.xbrl_parser_key = self.create_key("xbrl-parser@ons-companies-house-dev.iam.gserviceaccount.com")
 
 
-    @staticmethod
-    def create_key(service_account_email):
+    def create_key(self, service_account_email):
         """Creates a key for a service account."""
 
         credentials = service_account.Credentials.from_service_account_file(
-            filename="/home/dylan_purches/keys/data_key.json",
+            filename=self.iam_access_sa,
             scopes=['https://www.googleapis.com/auth/cloud-platform'])
 
         service = googleapiclient.discovery.build(
             'iam', 'v1', credentials=credentials)
 
         key = service.projects().serviceAccounts().keys().create(
-            name='projects/-/serviceAccounts/' + service_account_email, body={"lifetime":"300"}
+            name='projects/-/serviceAccounts/' + service_account_email, body={}
             ).execute()
 
         print('Created key: ' + key['name'])
         return key
+    
+    def delete_key(self, key):
+        """Deletes a service account key."""
+        try:
+            full_key_name = key["name"]
+        except:
+            full_key_name = "projects/" + key["project_id"] + "/serviceAccounts/" + key["client_email"] + "/keys/" + key["private_key_id"]
+
+
+        credentials = service_account.Credentials.from_service_account_file(
+            filename= self.iam_access_sa,
+            scopes=['https://www.googleapis.com/auth/cloud-platform'])
+
+        service = googleapiclient.discovery.build(
+            'iam', 'v1', credentials=credentials)
+
+        service.projects().serviceAccounts().keys().delete(
+            name=full_key_name).execute()
+
+        print('Deleted key: ' + full_key_name)
 
     @staticmethod
     def check_git_repo(filepath):
@@ -94,7 +124,7 @@ class GCPAuthenticator:
 
         with open(filepath, 'r') as f:
             data = f.read()
-        json_object = json.load(data)
+        json_object = json.loads(data)
 
         return json_object
 
@@ -104,4 +134,6 @@ if __name__ == "__main__":
 
     keys_folder = "/home/dylan_purches/repos/companies-house-big-data-project/test_dir"
 
-    authenticator = GCPAuthenticator()
+    authenticator = GCPAuthenticator("/home/dylan_purches/keys/data_key.json", "ons-companies-house-dev")
+
+    authenticator.get_sa_keys("/home/dylan_purches/keys")
