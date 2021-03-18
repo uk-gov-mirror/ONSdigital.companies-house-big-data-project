7c33462cd76b126ed44081a8f36e503035b1b96e
lewyse-lee <lewyse.lee@ons.gov.uk>
2021-03-18 15:05:45 +0000

    Imported config headers and changed how each are called in the main pipeline to make it more readable. Also incorperated the name changes of variables from the cha_pipeline.cfg file!

--- cha_pipeline.py
+++ cha_pipeline.py
@@ -33,66 +33,59 @@ pd.set_option("display.max_columns", 500)
 cfg = configparser.ConfigParser()
 cfg.read("cha_pipeline.cfg")
 
+# Config imports
+gcsfs_setup = cfg["gcsfs_setup"]
+cha_workflow = cfg["cha_workflow"]
+web_scraper_args = cfg["xbrl_web_scraper_args"]
+validator_args = cfg["xbrl_validator_args"]
+unpacker_args = cfg["xbrl_unpacker_args"]
+parser_args = cfg["xbrl_parser_args"]
+
 # Calling pipeline (based on booleans in config file)
 # -------------------------------------------------
 def main():
     print("-" * 50)
     #set GCP enviroment 
-    os.environ["PROJECT_ID"] = cfg['gcsfs_setup']["project_id"]
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = cfg['gcsfs_setup']['key']
-    fs = gcsfs.GCSFileSystem(project=cfg['gcsfs_setup']['project_id'], token=cfg['gcsfs_setup']['key'], cache_timeout=0)
+    os.environ["PROJECT_ID"] = gcsfs_setup["project_id"]
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = gcsfs_setup['key']
+    fs = gcsfs.GCSFileSystem(project=gcsfs_setup['project_id'], token=gcsfs_setup['key'], cache_timeout=0)
 
     # Execute module xbrl_web_scraper
-    if cfg['cha_workflow']['xbrl_web_scraper'] == str(True):
+    if cha_workflow['xbrl_web_scraper'] == str(True):
         print("XBRL web scraper running...")
-        print("Scraping XBRL data to:", cfg['xbrl_web_scraper_args']['x_scraper_dir_to_save'])
+        print("Scraping XBRL data to:", web_scraper_args['dir_to_save'])
 
         scraper = XbrlScraper()
-        scraper.scrape_webpage(cfg['xbrl_web_scraper_args']['x_scraper_url'],
-                               cfg['xbrl_web_scraper_args']['x_scraper_base_url'],
-                               cfg['xbrl_web_scraper_args']['x_scraper_dir_to_save'])
+        scraper.scrape_webpage(web_scraper_args['scraper_url'],
+                               web_scraper_args['domain'],
+                               web_scraper_args['dir_to_save'])
 
     # Validate xbrl data
-    if cfg['cha_workflow']['xbrl_validator'] == str(True):
+    if cha_workflow['xbrl_validator'] == str(True):
         print("Validating xbrl web scraped data...")
         validator = XbrlValidatorMethods(fs)
-        validator.validate_compressed_files(cfg['xbrl_validator_args']['x_validator_scraped_dir'])
+        validator.validate_compressed_files(validator_args['scraped_dir'])
 
     # Execute module xbrl_unpacker
-    if cfg['cha_workflow']['xbrl_unpacker'] == str(True):
+    if cha_workflow['xbrl_unpacker'] == str(True):
         print("XBRL unpacker running...")
-        print("Reading zip file from directory: ", cfg['xbrl_unpacker_args']['x_unpacker_file_source_dir'])
-        print("Unpacking zip file to directory: ", cfg['xbrl_unpacker_args']['x_unpacker_dir_to_save'])
+        print("Reading zip file from directory: ", unpacker_args['source_dir'])
+        print("Unpacking zip file to directory: ", unpacker_args['output_dir'])
         unpacker = DataProcessing(fs)
-        unpacker.extract_compressed_files(cfg['xbrl_unpacker_args']['x_unpacker_file_source_dir'],
-                                          cfg['xbrl_unpacker_args']['x_unpacker_dir_to_save'])
+        unpacker.extract_compressed_files(unpacker_args['x_unpacker_file_source_dir'],
+                                          unpacker_args['x_unpacker_dir_to_save'])
 
     # Execute module xbrl_parser
-    if cfg['cha_workflow']['xbrl_parser'] == str(True):
+    if cha_workflow['xbrl_parser'] == str(True):
         print("XBRL parser running...")
         parser_executer = XbrlParser(fs)
-        parser_executer.parse_files(cfg['xbrl_parser_args']['x_parser_process_quarter'],
-                                    cfg['xbrl_parser_args']['x_parser_process_year'],
-                                    cfg['xbrl_parser_args']['x_unpacked_data'],
-                                    cfg['xbrl_parser_args']['x_parser_custom_input'],
-                                    cfg['xbrl_parser_args']['x_parser_bq_location'],
-                                    cfg['xbrl_parser_args']['x_processed_csv'],
-                                    cfg['xbrl_parser_args']['x_parser_no_of_cores'])
-
-    # Execute module xbrl_csv_cleaner
-    if cfg['cha_workflow']['xbrl_csv_cleaner'] == str(True):
-        print("XBRL CSV cleaner running...")
-        XbrlCSVCleaner.clean_parsed_files(cfg['xbrl_csv_cleaner_args']['x_csv_cleaner_indir'],
-                                          cfg['xbrl_csv_cleaner_args']['x_csv_cleaner_outdir'])
-
-    # Append XBRL data on an annual or quarterly basis
-    if cfg['cha_workflow']['xbrl_file_appender'] == str(True):
-        print("XBRL appender running...")
-        appender = XbrlCsvAppender(fs)
-        appender.merge_files_by_year(cfg['xbrl_file_appender_args']['x_file_appender_indir'],
-                                     cfg['xbrl_file_appender_args']['x_file_appender_outdir'],
-                                     cfg['xbrl_file_appender_args']['x_file_appender_year'],
-                                     cfg['xbrl_file_appender_args']['x_file_appender_quarter'])
+        parser_executer.parse_files(parser_args['quarter'],
+                                    parser_args['year'],
+                                    parser_args['unpacked_data_dir'],
+                                    parser_args['custom_input'],
+                                    parser_args['bq_location'],
+                                    parser_args['processed_csv_dir'],
+                                    parser_args['no_of_cores'])
 
 if __name__ == "__main__":
     #do we want to incorperate time metrics in some of our print statements within modules?
