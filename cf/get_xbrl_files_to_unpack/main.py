a4b6607f99f1d603d938820ad257e24af50f5217
Dylan Purches <dylan.purches@ext.ons.gov.uk>
2021-03-26 14:49:23 +0000

    Added batching options to unpacker cloud functions

--- cf/get_xbrl_files_to_unpack/main.py
+++ cf/get_xbrl_files_to_unpack/main.py
@@ -18,20 +18,22 @@ def get_xbrl_files(event, context):
     zip_path = event["attributes"]["zip_path"]
     fs = gcsfs.GCSFileSystem(cache_timeout=0)
 
-    save_directory = "ons-companies-house-dev-xbrl-unpacked-data/cloud_functions_test/test_" + (zip_path.split("/")[-1]).split(".")[0]
+    save_directory = "ons-companies-house-dev-xbrl-unpacked-data/cloud_functions_test/" + (zip_path.split("/")[-1]).split(".")[0]
 
     batching_settings = pubsub_v1.types.BatchSettings(
         max_messages=1000
     )
     publisher = pubsub_v1.PublisherClient(batch_settings=batching_settings)
     topic_path = publisher.topic_path("ons-companies-house-dev", "xbrl_files_to_unpack")
-
+    
+    n = 1000
     with zipfile.ZipFile(fs.open(zip_path), 'r') as zip_ref:
-      names = zip_ref.namelist()[-1000:] 
+      zip_list = zip_ref.namelist()
+      names = [zip_list[i*n : (i+1)*n] for i in range((len(zip_list) + n - 1)//n)]
       for i, contentfilename in enumerate(names):
-        upload_path = save_directory + "/" + contentfilename
-        data = "Xbrl file to download: {}".format(contentfilename).encode("utf-8")
+        # upload_path = save_directory + "/" + contentfilename
+        data = str(contentfilename).encode("utf-8")
         future = publisher.publish(
-          topic_path, data, xbrl_path=contentfilename, export_path=upload_path, zip_path=zip_path
+          topic_path, data, save_directory=save_directory, zip_path=zip_path
         )
         future.add_done_callback(callback)
--- cf/xbrl_unpacker/main.py
+++ cf/xbrl_unpacker/main.py
@@ -11,23 +11,19 @@ def unpack_xbrl_file(event, context):
     """
     fs = gcsfs.GCSFileSystem(cache_timeout=0)
 
+    xbrl_list = eval(base64.b64decode(event['data']).decode('utf-8'))
+
     zip_path = event["attributes"]["zip_path"]
-    xbrl_path = event["attributes"]["xbrl_path"]
-    upload_path = event["attributes"]["export_path"]
-
-    print(f"Unpacking {xbrl_path} from zip")
-
-    overhead_t0 = time.time()
+    save_directory = event["attributes"]["save_directory"]
     
     with zipfile.ZipFile(fs.open(zip_path), 'r') as zip_ref:
-      content_file = zip_ref.read(xbrl_path)
-      overhead = time.time() - overhead_t0
-      process_t0 = time.time()
-      with fs.open(upload_path, 'wb') as f:
-        f.write(content_file)
-      fs.setxattrs(
-        upload_path,
-        content_type="text/"+xbrl_path.split(".")[-1]
-        )
-    process = time.time() - process_t0
-    print(f"Overhead: {overhead}s, Process: {process}s")
+      for xbrl_path in xbrl_list:
+        print(f"unpacking {xbrl_path} from {zip_path}")
+        upload_path = save_directory + "/" + xbrl_path
+        content_file = zip_ref.read(xbrl_path)
+        with fs.open(upload_path, 'wb') as f:
+            f.write(content_file)
+        fs.setxattrs(
+            upload_path,
+            content_type="text/"+xbrl_path.split(".")[-1]
+            )
