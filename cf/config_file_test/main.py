f73b7054656729cc36bcaf4cdc4e27e94144f058
Dylan Purches <dylan.purches@ext.ons.gov.uk>
2021-03-30 15:47:11 +0000

    Tried for config specified as a text file

--- cf/config_file_test/main.py
+++ cf/config_file_test/main.py
@@ -2,6 +2,6 @@ import configparser
 
 def test_config_file(event, context):
 
-    cfg = configparser.ConfigParser()
-    cfg.read("./../../cha_pipeline.cfg")
+    cfg = configparser.RawConfigParser()
+    cfg.read("../test_config.txt")
     print(cfg["auth_setup"]["iam_key"])
