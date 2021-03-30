32c851252b58c951cd068d82a008d361e4a0b40c
Dylan Purches <dylan.purches@ext.ons.gov.uk>
2021-03-30 14:17:50 +0000

    Fixed mistake in syntax

--- cf/config_file_test/main.py
+++ cf/config_file_test/main.py
@@ -1,6 +1,7 @@
 import configparser
 
 def test_config_file(event, context):
-    
-    cfg = configparser.read("../../cha_pipeline.cfg")
+
+    cfg = configparser.ConfigParser()
+    cfg.read("../../cha_pipeline.cfg")
     print(cfg["auth_setup"]["iam_key"])
