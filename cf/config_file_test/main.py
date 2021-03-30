4626cc562a2e6a3d8a7d89add77aea3343184f43
Dylan Purches <dylan.purches@ext.ons.gov.uk>
2021-03-30 15:43:05 +0000

    Tried different way of specifying config fp

--- cf/config_file_test/main.py
+++ cf/config_file_test/main.py
@@ -3,5 +3,5 @@ import configparser
 def test_config_file(event, context):
 
     cfg = configparser.ConfigParser()
-    cfg.read("../../cha_pipeline.cfg")
+    cfg.read("./../../cha_pipeline.cfg")
     print(cfg["auth_setup"]["iam_key"])
