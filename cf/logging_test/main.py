83784b81610186e318e447ed9ae49579a3596660
Dylan Purches <dylan.purches@ext.ons.gov.uk>
2021-04-07 10:06:07 +0000

    Added more clauses to test logging

--- cf/logging_test/main.py
+++ cf/logging_test/main.py
@@ -4,8 +4,10 @@ from google.cloud import logging as cloudlogging
 
 def send_test_logs(event, context):
 
+    logging.info("This is an info")
     logging.warning("This is a warning")
     logging.critical("This is critical")
+    logging.error("This is an error")
 
     t0 = time.time()
     log_client = cloudlogging.Client()
@@ -18,11 +20,15 @@ def send_test_logs(event, context):
     print(f"Logging setup took {t} seconds")
 
     t0 = time.time()
-    cloud_logger.info("This is info")
+    cloud_logger.info("CL: This is info")
     t = time.time() - t0
     print(f"LogOut took {t} s")
 
-    raise RuntimeError(
+    cloud_logger.error("CL: This is an error")
+    cloud_logger.critical("CL: This is critical")
+    cloud_logger.debug("CL: This a debug")
+    
+    raise TypeError(
         "This is to test error reporting"
     )
 
