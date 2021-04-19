51f0bfb32d73eb4e7653f62832113e36bf7e2b82
PeterJDerrick89 <peter.derrick@ons.gov.uk>
2021-04-19 07:37:20 +0000

    Removed double counting of files for batches

--- src/cf/xbrl_parser.py
+++ src/cf/xbrl_parser.py
@@ -37,7 +37,7 @@ class XbrlBatcher:
 
         # Retrieve a list of all file sizes for all files retrieved
         sizes = []
-        num_files = 100
+        num_files = 11
         #num_files = len(files) + 1
         for filename, n in zip(files[0:num_files], range(1, num_files + 1)):
             
@@ -46,11 +46,20 @@ class XbrlBatcher:
             
             sizes.append(self.fs.size(filename))
 
-        #authenticator.clean_up_keys()
-
         # Create list of files and their sizes and sort in descending order
         data_descending = sorted(zip(files, sizes), key=lambda t: t[1], reverse=True)
         data_ascending = sorted(data_descending, key=lambda t: t[1], reverse=False)
+
+        data_descending = data_descending[:(int)(len(data_descending) / 2)]
+        data_ascending = data_ascending[(int)(len(data_ascending) / 2):]
+
+        # If there is an odd number of files, append a dummy file to the end of the
+        # descending file list
+        if (num_files % 2) == 1:
+            data_descending.append(("", 0))
+
+        print(data_descending)
+        print(data_ascending)
 
         # Create batches of files using the Gauss approach of pairing the largest and
         # smallest files
@@ -78,7 +87,12 @@ class XbrlBatcher:
                 batch_size = 0
                 batch_count = 0
 
-            batch_list[current_batch] += [file_big[0], file_small[0]]
+            # Don't append the dummy file if it is present in the current file pair
+            if file_big[0] != "":
+                batch_list[current_batch] += [file_big[0], file_small[0]]
+            else:
+                batch_list[current_batch] += [file_small[0]]
+
             batch_size_list[current_batch].append(pair_size)
             batch_size += pair_size
             batch_count += 1
